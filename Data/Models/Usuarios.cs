e0a1f23ea5180fb09fd1eed35f16066005ddecb1
Christopher Acosta <christopheracostamejia97@gmail.com>
2022-10-31 17:18:18 -0600

    Debitos.cs

--- Data/Models/Usuarios.cs
+++ Data/Models/Usuarios.cs
@@ -3,7 +3,7 @@ namespace ColmadosAPP.Data.Models;
 public class Usuarios
 {
     public int Idusuario { get; set; }
-    public string NombreU { get; set; } = null!
-    public string Usuario { get; set; } = null!
-    public string clave { get; set;= nu } = null!
+    public string NombreU { get; set; } = null!;
+    public string Usuario { get; set; } = null!;
+    public string clave { get; set; } = null!;
 }
